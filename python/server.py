4bb715de7f2d4b28c2d0eccd5830b568b9ded18b
Gaurav <gaurav maheshwari>
2016-10-10 12:03:17 +0200

    Added files for verbalizing, word2vec and temaplate generation. All are running on the same server.

---
+++ python/server.py
@@ -0,0 +1,97 @@
+from bottle import route, run, get, post, request, response
+from pprint import pprint
+import json
+import requests
+from bs4 import BeautifulSoup
+
+import sparql_template_generator
+verbalizing_url = "http://km.aifb.kit.edu/projects/spartiqulator/v5/verbalize.pl?"
+
+
+#generating sparql templates
+@post('/templates') 
+def templates():
+	data = request.json
+	entities = [x['ent'] for x in data['entities']]
+	predicates = [x['pred'] for x in data['predicates']]
+	sparqlgen = sparql_template_generator.SPARQL_Template_Generator(entities,predicates)
+
+	#making a dictionary of the questions and answers
+	info = []
+
+	for tup in sparqlgen.sparqls_answers:
+		info += [{'sparql':tup[0],'answer':tup[1]}]
+	response.content_type = 'application/json'
+	return json.dumps(info)
+
+
+#for finding similarity between words using word2vec
+@post('/similarity')
+def hello():
+	word1=request.forms.get('word1')
+	word2=request.forms.get('word2')
+	# word1 = request.forms.get("word1")
+	# word2 = request.forms.get("word2")
+	print word1 + " "+word2
+	try:
+		similarity_value = model.similarity(word1, word2)
+	except Exception as e:
+		# print traceback.print_exc
+		similarity_value = str(-2)
+	print "hello world"
+	print similarity_value
+	return str(similarity_value)
+
+
+#for verbalizing queries
+@post('/verbalize')
+def verbalize():
+	# query = '''PREFIX dbo: <http://dbpedia.org/ontology/>
+	# PREFIX res: <http://dbpedia.org/resource/>
+	# PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+	# PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+	# SELECT DISTINCT ?uri ?string 
+	# WHERE {
+	# 	?uri rdf:type dbo:Film .
+	#         ?uri dbo:starring res:Julia_Roberts .
+	#         ?uri dbo:starring res:Richard_Gere.
+	# 	OPTIONAL {?uri rdfs:label ?string . FILTER (lang(?string) = 'en') }
+	# }'''
+	query = request.forms.get('query')
+
+	payload = {'sparql':query}
+	url = verbalizing_url
+	r = requests.get(url,params=payload)
+
+	html_content = r.content
+	# print r.content
+	soup = BeautifulSoup(html_content)
+	question = ""
+
+	try:
+		input_tag = soup.find_all("input")
+		for x in input_tag:
+			counter = 0
+			try:
+				if x.attrs["name"] == "verbalization":
+					print x.attrs["value"]
+					question = x.attrs["value"]
+					counter = 1
+			except:
+				continue
+			if counter == 1:
+				break
+	except:
+		print "no input tag found ! HTML content might have been changed"
+		return str(-2)
+	if question:
+		print "verbalized"
+		return str(question)
+	else:
+		print "not found"
+		return str(-2)
+
+run(host='localhost', port=8080, debug=True)
+
+
+
