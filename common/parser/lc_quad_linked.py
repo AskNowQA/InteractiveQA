004835f32c21a8040b9f534d1eae1a21e426ba4e
hamidzafar <hamid.zafar.tud@gmail.com>
2018-02-14 17:09:26 +0100

    dataset parsers

---
+++ common/parser/lc_quad_linked.py
@@ -0,0 +1,40 @@
+import json
+import re
+from common.container.qapair import QApair
+from common.container.uri import Uri
+from common.kb.dbpedia import DBpedia
+
+
+class LC_Qaud_Linked:
+    def __init__(self, path="./data/LC-QuAD/linked.json"):
+        self.raw_data = []
+        self.qapairs = []
+        self.path = path
+        self.parser = LC_Qaud_LinkedParser()
+
+    def load(self):
+        with open(self.path) as data_file:
+            self.raw_data = json.load(data_file)
+
+    def parse(self):
+        for raw_row in self.raw_data:
+            self.qapairs.append(
+                QApair(raw_row["question"], raw_row["sparql_query"], raw_row, raw_row["id"],
+                       self.parser))
+
+    def print_pairs(self, n=-1):
+        for item in self.qapairs[0:n]:
+            print item
+            print ""
+
+
+class LC_Qaud_LinkedParser():
+    def __init__(self):
+        self.kb = DBpedia()
+
+    def parse_question(self, raw_question):
+        return raw_question
+
+    def parse_sparql(self, raw_query):
+        uris = [Uri(raw_uri, self.kb.parse_uri) for raw_uri in re.findall('(<[^>]*>|\?[^ ]*)', raw_query)]
+        return raw_query, True, uris
