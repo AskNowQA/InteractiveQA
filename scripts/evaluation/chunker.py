e266b6d1081b3ccb175cdaafffa61828c9796769
hamidzafar <hamid.zafar.tud@gmail.com>
2018-02-14 14:16:18 +0100

    Evalaute ClassifierChunkParser

--- scripts/evaluation/chunker.py
+++ scripts/evaluation/chunker.py
@@ -1,16 +1,37 @@
+from common.utility.utils import Utils
+from common.component.chunker.SENNAChunker import SENNAChunker
+from common.component.chunker.classifierChunkParser import ClassifierChunkParser
 import pickle as pk
 import nltk
-from common.component.chunker.SENNAChunker import SENNAChunker
+import os
 
 if __name__ == "__main__":
+    test_sentence = "Was Winston Churchill the prime minister of Selwyn Lloyd?"
+    base_dir = "../../"
+    model_dir = os.path.join(base_dir, "models")
+    Utils.makedirs(model_dir)
     with open('../../data/LC-QUAD/linked2843_IOB.pk') as data_file:
         dataset = pk.load(data_file)
     idx = int(len(dataset) * 0.9)
-    idx = -1
+    # idx = -1
     train_sents = dataset[:idx]
     test_sents = dataset[idx + 1:]
-    test_sents = [nltk.chunk.conlltags2tree(item) for item in test_sents]
+    test_sents_tree = [nltk.chunk.conlltags2tree(item) for item in test_sents]
 
-    chunker = SENNAChunker()
-    print chunker.evaluate(test_sents)
-    print chunker.shallowParse("Was Winston Churchill the prime minister of Selwyn Lloyd?")
+    SENNA_chunker = SENNAChunker()
+    print "SENNA Chunker"
+    print SENNA_chunker.evaluate(test_sents_tree)
+
+
+    tagger_filename = os.path.join(model_dir, "ClassifierChunkParser.tagger.model")
+    # if os.path.exists(tagger_filename):
+    #     os.remove(tagger_filename)
+
+    classifier_chunker = ClassifierChunkParser(train_sents, tagger_filename)
+    print "Classifier Chunker"
+    # print classifier_chunker.evaluate(test_sents_tree)
+
+
+
+    print SENNA_chunker.parse(nltk.pos_tag(nltk.word_tokenize(test_sentence)))
+    print classifier_chunker.parse(nltk.pos_tag(nltk.word_tokenize(test_sentence)))
