76f8f860431791e063bcac7126aa5fc812a721ed
hamidzafar <hamid.zafar.tud@gmail.com>
2018-02-14 15:31:46 +0100

    a wrapper for EARL

---
+++ common/component/linker/EARL.py
@@ -0,0 +1,56 @@
+from common.utility.utils import Utils
+import json
+import os
+
+
+class EARL:
+    def __init__(self, endpoint='http://localhost:5000/processQuery', cache_path="", use_cache=True):
+        self.endpoint = endpoint
+        self.cache_path = cache_path
+        Utils.makedirs(cache_path)
+        self.cache_path = os.path.join(cache_path, "earl.cache")
+        self.use_cache = use_cache
+        self.cache = {}
+        if self.use_cache:
+            self.__load_cache()
+
+    def __load_cache(self):
+        try:
+            with open(self.cache_path) as cache_file:
+                self.cache = json.load(cache_file)
+        except:
+            self.cache = {}
+
+    def __save_cache(self):
+        try:
+            with open(self.cache_path, "w") as cache_file:
+                json.dump(self.cache, cache_file)
+            return True
+        except:
+            return False
+
+    def __hit_endpoint(self, question, chunks):
+        id = question
+        input = {'nlquery': question}
+        if chunks is not None and isinstance(chunks, list) and len(chunks) > 0:
+            input['chunks'] = chunks
+            id += "".join(chunks)
+
+        if id not in self.cache or not self.use_cache:
+            self.cache[id] = Utils.call_web_api(self.endpoint, input)
+            self.__save_cache()
+        return self.cache[id]
+
+    def chunk(self, question):
+        return []
+
+    def link_entities(self, question, chunks=None):
+        return self.__hit_endpoint(question, chunks)["entities"]
+
+    def link_relations(self, question, chunks=None):
+        return self.__hit_endpoint(question, chunks)["relations"]
+
+    def link_entities_relations(self, question):
+        chunks = self.chunk(question)
+        return {"relations": self.link_relations(question, chunks),
+                "entities": self.link_entities(question, chunks)}
--- common/utility/utils.py
+++ common/utility/utils.py
@@ -2,6 +2,8 @@ from __future__ import division
 from __future__ import print_function
 import os
 import logging.config
+import json
+import urllib2
 
 
 class Struct(object): pass
@@ -15,14 +17,7 @@ class Utils:
         return None
 
     @staticmethod
-    def setup_logging(
-            default_path='logging.json',
-            default_level=logging.INFO,
-            env_key='LOG_CFG'
-    ):
-        """Setup logging configuration
-
-        """
+    def setup_logging(default_path='logging.json', default_level=logging.INFO, env_key='LOG_CFG'):
         path = default_path
         value = os.getenv(env_key, None)
         if value:
@@ -33,3 +28,15 @@ class Utils:
             logging.config.dictConfig(config)
         else:
             logging.basicConfig(level=default_level)
+
+    @staticmethod
+    def call_web_api(endpoint, input):
+        req = urllib2.Request(endpoint)
+        req.add_header('Content-Type', 'application/json')
+        # try:
+        response = urllib2.urlopen(req, json.dumps(input))
+        response = response.read()
+        response = json.loads(response)
+        return response
+        # except:
+        #     return None
