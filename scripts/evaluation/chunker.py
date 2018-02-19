0f8861311a8a62c4084f63341022ed8b79c90a09
hamidzafar <hamid.zafar.tud@gmail.com>
2018-02-19 13:12:11 +0100

    add goldchunker

--- scripts/evaluation/chunker.py
+++ scripts/evaluation/chunker.py
@@ -1,26 +1,33 @@
 from common.utility.utils import Utils
 from common.component.chunker.SENNAChunker import SENNAChunker
 from common.component.chunker.classifierChunkParser import ClassifierChunkParser
+from common.component.chunker.goldChunker import GoldChunker
 import pickle as pk
 import nltk
 import os
 
 if __name__ == "__main__":
-    test_sentence = "Was Winston Churchill the prime minister of Selwyn Lloyd?"
+    test_sentence = "Was winston churchill the prime minister of Selwyn Lloyd?"
     base_dir = "../../"
     model_dir = os.path.join(base_dir, "models")
     Utils.makedirs(model_dir)
     with open('../../data/LC-QUAD/linked2843_IOB.pk') as data_file:
-        dataset = pk.load(data_file)
+        orginal_dataset = pk.load(data_file)
+
+    dataset = [item[1:] for item in orginal_dataset]
     idx = int(len(dataset) * 0.9)
     # idx = -1
     train_sents = dataset[:idx]
     test_sents = dataset[idx + 1:]
     test_sents_tree = [nltk.chunk.conlltags2tree(item) for item in test_sents]
 
+    gold_chunker = GoldChunker({item[0]: item[1:] for item in orginal_dataset})
+    print "Gold Chunker"
+    print gold_chunker.evaluate(test_sents_tree)
+
     SENNA_chunker = SENNAChunker()
-    print "SENNA Chunker"
-    print SENNA_chunker.evaluate(test_sents_tree)
+    # print "SENNA Chunker"
+    # print SENNA_chunker.evaluate(test_sents_tree)
 
 
     tagger_filename = os.path.join(model_dir, "ClassifierChunkParser.tagger.model")
@@ -29,9 +36,7 @@ if __name__ == "__main__":
 
     classifier_chunker = ClassifierChunkParser(train_sents, tagger_filename)
     print "Classifier Chunker"
-    # print classifier_chunker.evaluate(test_sents_tree)
+    print classifier_chunker.evaluate(test_sents_tree)
 
-
-
-    print SENNA_chunker.parse(nltk.pos_tag(nltk.word_tokenize(test_sentence)))
-    print classifier_chunker.parse(nltk.pos_tag(nltk.word_tokenize(test_sentence)))
+    print SENNA_chunker.get_phrases(test_sentence)
+    print classifier_chunker.get_phrases(test_sentence)
