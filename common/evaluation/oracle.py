c005b98cbcc32d7d98c0c6bfca33d3be7f91510d
hamidzafar <hamid.zafar.tud@gmail.com>
2018-04-24 15:11:09 +0200

    complete oracle for LC-QuAD

--- common/evaluation/oracle.py
+++ common/evaluation/oracle.py
@@ -2,5 +2,5 @@ class Oracle:
     def __init__(self):
         pass
 
-    def answer(self, io):
-        return True
+    def answer(self, qapair, io):
+        return len([uri.uri for uri in qapair.sparql.uris if uri.uri == io.value["uri"]]) > 0
--- scripts/execution/pipeline.py
+++ scripts/execution/pipeline.py
@@ -82,13 +82,8 @@ class IQAPipeline:
 
             interaction_options = InteractionOptions(outputs[2], dataset.parser.parse_sparql)
             while interaction_options.has_interaction():
-                io_idx = interaction_options.interactionWithMaxInformationGain()
-                interaction_options.update(io_idx, oracle.answer(io_idx))
-            # if any([len(item['queries']) > 0 for item in outputs[cmpnt_idx]]):
-            #     for item in outputs:
-            #         print outputs[item]
-
-            # print "*" * 20
+                io = interaction_options.interactionWithMaxInformationGain()
+                interaction_options.update(io, oracle.answer(qapair, io))
 
 
 if __name__ == "__main__":
