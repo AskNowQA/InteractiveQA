6987008663d5a45440adffe6d198db3ea6f280e2
hamidzafar <hamid.zafar.tud@gmail.com>
2018-04-25 15:37:29 +0200

    support c3 in oracle eval

--- common/evaluation/oracle.py
+++ common/evaluation/oracle.py
@@ -3,7 +3,15 @@ class Oracle:
         pass
 
     def answer(self, qapair, io):
-        if io.value['type'] == 'linked':
+        if io.value == 'boolean':
+            return "ask " in qapair.sparql.query.lower()
+        elif io.value == 'count':
+            return "count(" in qapair.sparql.query.lower()
+        elif io.value == 'list':
+            return not ("ask " in qapair.sparql.query.lower() or "count(" in qapair.sparql.query.lower())
+        elif io.value['type'] == 'linked':
             return len([uri.uri for uri in qapair.sparql.uris if uri.uri == io.value["uri"]]) > 0
-        else:
+        elif io.value['type'] == 'type':
             return len([uri for uri in qapair.sparql.uris if uri.is_entity() and io.value["uri"] in uri.types]) > 0
+        else:
+            return False
