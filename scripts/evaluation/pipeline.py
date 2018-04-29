445dfa68f6093c34470b548c655f47e57a0e0c3d
hamidzafar <hamid.zafar.tud@gmail.com>
2018-04-29 14:20:35 +0200

    eval pipeline according to three strategies: InformationGain, OptionGain, Probability

---
+++ scripts/evaluation/pipeline.py
@@ -0,0 +1,66 @@
+from common.pipeline import IQAPipeline
+from common.parser.lc_quad_linked import LC_Qaud_Linked
+from common.container.interactionOptions import InteractionOptions
+from common.evaluation.oracle import Oracle
+from common.kb.dbpedia import DBpedia
+from common.utility.stats import Stats
+from tqdm import tqdm
+import argparse
+import os
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run pipeline')
+    parser.add_argument("--base_path", help="base path", default="../../", dest="base_path")
+    parser.add_argument("--dataset", help="input Q/A dataset", default="data/LC-QuAD/linked.json", dest="dataset")
+    parser.add_argument("--model", help="path to model", default="models/ClassifierChunkParser.tagger.model",
+                        dest="model")
+    parser.add_argument("--gold_chunk", help="path to gold chunked dataset", default="data/LC-QuAD/linked2843_IOB.pk",
+                        dest="gold_chunk")
+    args = parser.parse_args()
+
+    dataset = [{"question": "Name the municipality of Roberto Clemente Bridge ?"}]
+    dataset = LC_Qaud_Linked(os.path.join(args.base_path, args.dataset))
+
+    kb = DBpedia(cache_path=os.path.join(args.base_path, "caches/"), use_cache=True)
+    parse_sparql = dataset.parser.parse_sparql
+    pipeline = IQAPipeline(args, kb, parse_sparql)
+    oracle = Oracle()
+
+    interaction_types = [[False, True], [True, True]]
+    strategies = ['InformationGain', 'OptionGain', 'Probability']
+    w = 1
+
+    stats = {('IQA-AO' if all(type) else 'IQA-SO') + strategy: Stats() for strategy in strategies for type
+             in interaction_types}
+    stats['general'] = Stats()
+    qid = 0
+    for qapair in tqdm(dataset.qapairs):
+        if 'municipality' not in qapair.question.text:
+            continue
+        stats['general'].inc("total")
+        outputs = pipeline.run(qapair)
+        for interaction_type in interaction_types:
+            interaction_type_str = 'IQA-AO' if all(interaction_type) else 'IQA-SO'
+            for strategy in strategies:
+                interaction_options = InteractionOptions(outputs[2], parse_sparql, kb, *interaction_type)
+                while interaction_options.has_interaction() and \
+                        not oracle.validate_query(qapair, interaction_options.queryWithMaxProbability()):
+
+                    if strategy == 'InformationGain':
+                        io = interaction_options.interactionWithMaxInformationGain()
+                    elif strategy == 'OptionGain':
+                        io = interaction_options.interactionWithMaxOptionGain(w)
+                    elif strategy == 'Probability':
+                        io = interaction_options.interactionWithMaxProbability()
+
+                    interaction_options.update(io, oracle.answer(qapair, io))
+                    stats[interaction_type_str + strategy].inc(str(qid))
+
+                if oracle.validate_query(qapair, interaction_options.queryWithMaxProbability()):
+                    stats[interaction_type_str + strategy].inc(str(qid) + "+correct")
+                else:
+                    stats[interaction_type_str + strategy].inc(str(qid) + "-incorrect")
+        qid += 1
+
+    print stats
+    stats.save('output/stats.json')
