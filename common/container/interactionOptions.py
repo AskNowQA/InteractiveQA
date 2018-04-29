3179e37fcd27a3e93b9e38c0a0502cc75b94bbb9
hamidzafar <hamid.zafar.tud@gmail.com>
2018-04-29 14:17:11 +0200

    Get potential entity/relation directly from the output of their component

--- common/container/interactionOptions.py
+++ common/container/interactionOptions.py
@@ -1,47 +1,40 @@
 from common.container.interactionOption import InteractionOption
 from common.utility.uniqueList import UniqueList
 from common.kb.dbpedia import DBpedia
-import itertools
+from common.container.linkeditem import LinkedItem
 import math
 
 
 class InteractionOptions:
-    def __init__(self, complete_interpretation_space, parser, kb=DBpedia()):
+    def __init__(self, complete_interpretation_space, parser, kb=DBpedia(), c2=True, c3=True):
         self.dic = dict()
         self.kb = kb
         self.complete_interpretation_space = complete_interpretation_space
         self.all_queries = UniqueList()
         self.all_ios = UniqueList()
         for output in complete_interpretation_space:
-            for query in output['queries']:
-                query['removed'] = False
-                self.all_queries.addIfNotExists(query)
-                self.add(InteractionOption('type', query['type'], query))
-                _, _, uris = parser(query['query'])
-                for uri in uris:
-                    if uri.is_generic():
-                        continue
-                    raw_uri = uri.uri
-                    found = False
-                    for entity_relation in itertools.chain(output['entities'], output['relations']):
-                        for item in entity_relation['uris']:
-                            if item['uri'] == raw_uri:
-                                io = InteractionOption(str(entity_relation['surface']), item, query)
-                                io.value['type'] = 'linked'
-                                self.add(io)
-                                found = True
-                                break
-                        if found:
-                            break
+            for linked_item_type in ['entities', 'relations']:
+                if linked_item_type in output:
+                    for item in output[linked_item_type]:
+                        io = InteractionOption(item.surface_form, item, item.related_queries, 'linked')
+                        self.add(io)
 
-        for item in self.dic:
-            for io in self.dic[item]:
-                if isinstance(io.value, dict) and io.value['type'] == 'linked' and '/resource/' in io.value['uri']:
-                    for type in kb.get_types(io.value['uri']):
-                        self.add(InteractionOption(io.id, {'uri': type,
-                                                           'confidence': io.value['confidence'],
-                                                           'type': 'type'},
-                                                   io.related_queries))
+            if 'queries' in output:
+                for query in output['queries']:
+                    query['removed'] = False
+                    self.all_queries.addIfNotExists(query)
+                    if c3:
+                        self.add(InteractionOption('type', query['type'], query, 'type'))
+
+        if c2:
+            for item in self.dic:
+                for io in self.dic[item]:
+                    if io.type == 'linked' and isinstance(io.value, LinkedItem) and io.value.uris[0].is_entity():
+                        for type in kb.get_types(io.value.uris[0].uri):
+                            self.add(InteractionOption(io.id, LinkedItem(io.id, [type]), io.related_queries, 'type'))
+                            # {'uri': type,
+                            #  'confidence': io.value['confidence'],
+                            #  'type': 'type'}
 
         self.__remove_items_contained_in_others()
         self.__remove_single_options()
@@ -84,19 +77,19 @@ class InteractionOptions:
         else:
             self.dic[interactionOption.id] = UniqueList([interactionOption])
 
-    def all_active_queries(self):
+    def __all_active_queries(self):
         return [query for query in self.all_queries if not query['removed']]
 
-    def all_active_ios(self):
+    def __all_active_ios(self):
         return [io for io in self.all_ios if not io.removed()]
 
-    def filter_interpretation_space(self, interaction_option):
+    def __filter_interpretation_space(self, interaction_option):
         positive = [query for query in interaction_option.related_queries if not query['removed']]
-        negetive = [query for query in self.all_active_queries() if query not in positive]
+        negetive = [query for query in self.__all_active_queries() if query not in positive]
 
         return positive, negetive
 
-    def entropy(self, interpretation_space, s=None):
+    def __entropy(self, interpretation_space, s=None):
         if s is None:
             s = sum([q['complete_confidence'] for q in interpretation_space])
         plogs = []
@@ -105,33 +98,62 @@ class InteractionOptions:
             plogs.append(p * math.log(p, 2))
         return -sum(plogs)
 
-    def averageEntropy(self, interaction_option):
-        S_sum = sum([q['complete_confidence'] for q in self.all_active_queries()])
-        queries_contain_io, queries_not_contain_io = self.filter_interpretation_space(interaction_option)
-        entropy_positive = self.entropy(queries_contain_io, S_sum)
-        entropy_negative = self.entropy(queries_not_contain_io, S_sum)
+    def __averageEntropy(self, interaction_option):
+        S_sum = sum([q['complete_confidence'] for q in self.__all_active_queries()])
+        queries_contain_io, queries_not_contain_io = self.__filter_interpretation_space(interaction_option)
+        entropy_positive = self.__entropy(queries_contain_io, S_sum)
+        entropy_negative = self.__entropy(queries_not_contain_io, S_sum)
         p_entropy_positive = sum([q['complete_confidence'] for q in queries_contain_io]) / S_sum
         p_entropy_negative = sum([q['complete_confidence'] for q in queries_not_contain_io]) / S_sum
 
         return p_entropy_positive * entropy_positive + p_entropy_negative * entropy_negative
 
-    def interactionWithMaxInformationGain(self):
-        entropy = self.entropy(self.all_active_queries())
+    def __informationGain(self):
+        entropy = self.__entropy(self.__all_active_queries())
         information_gains = []
 
-        for io in self.all_active_ios():
-            information_gains.append(entropy - self.averageEntropy(io))
+        for io in self.__all_active_ios():
+            information_gains.append([io, entropy - self.__averageEntropy(io)])
 
+        return information_gains
+
+    def interactionWithMaxInformationGain(self):
+        information_gains = [item[1] for item in self.__informationGain()]
         io, idx = max([(v, i) for i, v in enumerate(information_gains)])
-        return self.all_active_ios()[idx]
+        return self.__all_active_ios()[idx]
+
+    def interactionWithMaxOptionGain(self, w):
+        information_gains = self.__informationGain()
+        option_gains = [math.pow(item[0].usability(), w) * item[1] for item in information_gains]
+        io, idx = max([(v, i) for i, v in enumerate(option_gains)])
+        return self.__all_active_ios()[idx]
+
+    def interactionWithMaxProbability(self):
+        S_sum = sum([q['complete_confidence'] for q in self.__all_active_queries()])
+        probabilities = []
+
+        for io in self.__all_active_ios():
+            p = 0
+            for query in io.related_queries:
+                p += query['complete_confidence'] / S_sum
+            probabilities.append(p)
+
+        io, idx = max([(v, i) for i, v in enumerate(probabilities)])
+        return self.__all_active_ios()[idx]
+
+    def queryWithMaxProbability(self):
+        confidences = [q['complete_confidence'] for q in self.__all_active_queries()]
+
+        _, idx = max([(v, i) for i, v in enumerate(confidences)])
+        return self.__all_active_queries()[idx]
 
     def has_interaction(self):
-        if len(self.all_active_queries()) > 1:
-            return len(self.all_active_ios()) > 1
+        if len(self.__all_active_queries()) > 1:
+            return len(self.__all_active_ios()) > 1
         return False
 
     def update(self, io, value):
-        queries_contain_io, queries_not_contain_io = self.filter_interpretation_space(io)
+        queries_contain_io, queries_not_contain_io = self.__filter_interpretation_space(io)
         if value:
             for query in queries_not_contain_io:
                 query['removed'] = True
@@ -142,29 +164,10 @@ class InteractionOptions:
         # Remove current IO
         io.set_removed(True)
         # remove IOs which have no active query
-        for io in self.all_active_ios():
+        for io in self.__all_active_ios():
             if not io.removed():
                 io.set_removed(all([query['removed'] for query in io.related_queries]))
 
     def __iter__(self):
         for item in self.dic:
             yield item
-
-
-if __name__ == "__main__":
-    ios = InteractionOptions([], None, None)
-
-    q1 = {'name': 'cq1', 'complete_confidence': 0.2, 'removed': False}
-    q2 = {'name': 'cq2', 'complete_confidence': 0.5, 'removed': False}
-    q3 = {'name': 'cq3', 'complete_confidence': 0.7, 'removed': False}
-    io1 = InteractionOption("o1", None, [q1, q2])
-    io2 = InteractionOption("o2", None, [q1, q3])
-    io3 = InteractionOption("o3", None, [q2, q3])
-    ios.all_queries.extend([q1, q2, q3])
-    ios.all_ios.extend([io1, io2, io3])
-
-    while ios.has_interaction():
-        io = ios.interactionWithMaxInformationGain()
-        ios.update(io, True)
-
-    print "done"
