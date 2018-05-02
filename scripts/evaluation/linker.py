29692a7c440215aa839431f7abd362b03a44592a
hamidzafar <hamid.zafar.tud@gmail.com>
2018-05-02 16:26:32 +0200

    refactor to be able to use it as an evaluator elsewhere

--- scripts/evaluation/linker.py
+++ scripts/evaluation/linker.py
@@ -8,78 +8,70 @@ from tqdm import tqdm
 import logging
 
 
-def compare(list1, list2):
-    miss_match = False
-    for item in list1:
-        target_uri = item.uris[0]
-        found = False
-        for e2_item in list2:
-            if target_uri in e2_item.uris:
-                found = True
+class LinkerEvaluator:
+    def __init__(self, gold_linker):
+        self.gold_linker = gold_linker
+
+    def __compare_list(self, list1, list2):
+        miss_match = False
+        for item in list1:
+            target_uri = item.uris[0]
+            found = False
+            for e2_item in list2:
+                if target_uri in e2_item.uris:
+                    found = True
+                    break
+            if not found:
+                miss_match = True
                 break
-        if not found:
-            miss_match = True
-            break
-    return miss_match
+        return miss_match
+
+    def compare(self, qapair, e2, r2):
+        e1, r1 = self.gold_linker.do(qapair)
+        # double_relation = len(r1) > len(set([p.raw_uri for u in r1 for p in u.uris]))
+        if len(e2) == 0:
+            return '-no_entity'
+        if len(r2) == 0:
+            return '-no_relation'
+
+        if len(e2) == 0 and len(r2) == 0:
+            return '-no_input'
+
+        if len(e1) != len(e2):
+            return '-len_entity'
+
+        if len(r1) != len(r2):
+            return '-len_relation'
+
+        e = self.__compare_list(e1, e2)
+        r = self.__compare_list(r1, r2)
+        if e:
+            return '-miss_matched_entity'
+        if r:
+            return '-miss_matched_relation'
+
+        return "+matched"
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     Utils.setup_logging()
 
     parser = argparse.ArgumentParser(description='Evaluate linker')
-    parser.add_argument("--path", help="input dataset", default="../../data/LC-QuAD/linked2843.json", dest="ds_path")
-    parser.add_argument("--linker", help="linker output path", default="../../data/LC-QuAD/EARL/output_gold.json",
-                        dest="linker_path")
+    parser.add_argument('--path', help='input dataset', default='../../data/LC-QuAD/linked2843.json', dest='ds_path')
+    parser.add_argument('--linker', help='linker output path', default='../../data/LC-QuAD/EARL/output_gold.json',
+                        dest='linker_path')
     args = parser.parse_args()
 
     stats = Stats()
-
+    evaluator = LinkerEvaluator(GoldLinker())
     ds = LC_Qaud_Linked(args.ds_path)
-
-    goldLinker = GoldLinker()
     earl = EARL(args.linker_path)
 
     for qapair in tqdm(ds.qapairs):
-        e1, r1 = goldLinker.do(qapair)
         e2, r2 = earl.do(qapair, force_gold=False, top=100)
-        double_relation = len(r1) > len(set([p.raw_uri for u in r1 for p in u.uris]))
-        if len(e2) == 0:
-            stats.inc("earl_no_entity")
-        if len(r2) == 0:
-            stats.inc("earl_no_relation")
-
-        if len(e2) == 0 and len(r2) == 0:
-            stats.inc("earl_no")
-
-        if len(e1) == len(e2):
-            stats.inc("len_entity")
-        # else:
-        #     print "not len_entity"
-
-        if len(r1) == len(r2):
-            stats.inc("len_relation")
-        # else:
-        #     print "not len_relation"
-
-        if len(e1) == len(e2) and len(r1) == len(r2):
-            stats.inc("len_both")
-
-        e = compare(e1, e2)
-        r = compare(r1, r2)
-        if not e:
-            stats.inc("matched_entity")
-        if not r:
-            stats.inc("matched_relation")
-        if not e and not r:
-            stats.inc("matched_both")
-
-        if len(e1) == len(e2) and len(r1) == len(r2) and not e and not r:
-            stats.inc("matched_both_and_len")
-        # else:
-        #     print qapair.question
-
-        stats.inc("total")
+        stats.inc(evaluator.compare(qapair, e2, r2))
+        stats.inc('total')
 
     print
     print stats
