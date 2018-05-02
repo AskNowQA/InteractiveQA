a234f96d78fd45335621c9100cab84a9b94406d7
hamidzafar <hamid.zafar.tud@gmail.com>
2018-05-02 16:26:05 +0200

    add convert_to_linked_item

--- common/container/linkeditem.py
+++ common/container/linkeditem.py
@@ -1,3 +1,6 @@
+from common.container.uri import Uri
+
+
 class LinkedItem:
     def __init__(self, surface_form, uris, related_queries=None):
         self.surface_form = surface_form
@@ -33,3 +36,9 @@ class LinkedItem:
         if isinstance(other, LinkedItem):
             return self.surface_form == other.surface_form and self.uris == other.uris
         return False
+
+    @staticmethod
+    def convert_to_linked_item(items, uri_parser):
+        return [LinkedItem(item['surface'],
+                           [Uri(uri['uri'], uri_parser, uri['confidence']) for uri in item['uris']]) for item in
+                items]
