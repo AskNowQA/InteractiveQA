a8e911084331d2749af34621c1b453892bde769f
hamidzafar <hamid.zafar.tud@gmail.com>
2018-05-03 17:00:06 +0200

    make lc_quad_linked compatible with old json format

--- common/parser/lc_quad_linked.py
+++ common/parser/lc_quad_linked.py
@@ -6,7 +6,7 @@ from common.kb.dbpedia import DBpedia
 
 
 class LC_Qaud_Linked:
-    def __init__(self, path="./data/LC-QuAD/linked.json"):
+    def __init__(self, path='./data/LC-QuAD/linked.json'):
         self.raw_data = []
         self.qapairs = []
         self.path = path
@@ -20,18 +20,16 @@ class LC_Qaud_Linked:
 
     def parse(self):
         for raw_row in self.raw_data:
-            self.qapairs.append(
-                QApair(raw_row["question"],
-                       raw_row["sparql_query"].replace('https://www.w3.org/1999/02/22-rdf-syntax-ns#type',
-                                                       'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
-                       raw_row,
-                       raw_row["id"],
-                       self.parser))
+            question = raw_row['question'] if 'question' in raw_row else raw_row['corrected_question']
+            query = raw_row['sparql_query'].replace('https://www.w3.org/1999/02/22-rdf-syntax-ns#type',
+                                                    'http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
+            id = raw_row['id'] if 'id' in raw_row else raw_row['_id']
+            self.qapairs.append(QApair(question, query, raw_row, id, self.parser))
 
     def print_pairs(self, n=-1):
         for item in self.qapairs[0:n]:
             print item
-            print ""
+            print ''
 
 
 class LC_Qaud_LinkedParser():
