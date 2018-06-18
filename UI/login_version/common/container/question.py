44c4d95c52b95b4be7de8bfda96dbb0717ac3770
hamidzafar <hamid.zafar.tud@gmail.com>
2018-06-18 14:22:25 +0200

    remove old UI

--- UI/login_version/common/container/question.py
+++
@@ -1,8 +0,0 @@
-class Question:
-    def __init__(self, raw_question, parser):
-        self.text = ""
-        self.raw_question = raw_question
-        self.text = parser(raw_question)
-
-    def __str__(self):
-        return self.text.encode("ascii", "ignore")
