f4f675600f2c1de3bcc95ef0bf33282f2faa8758
hamidzafar <hamid.zafar.tud@gmail.com>
2018-06-21 11:18:01 +0200

    refactor

--- UI/app.py
+++ UI/app.py
@@ -127,7 +127,7 @@ def survey():
         data['strategy'] = flask.request.values['strategy']
 
     result = Utils.call_web_api('http://127.0.0.1:5002/iqa/ui/v1.0/start', data)
-    result['sparql2nl'] = sparql2nl(result['query'])
+    result = reformat(result)
     return render_template('survey.html', data=result)
 
 
@@ -136,17 +136,20 @@ def survey():
 def interact():
     data = {'userid': current_user.username, 'answer': flask.request.values['answer']}
     result = Utils.call_web_api('http://127.0.0.1:5002/iqa/ui/v1.0/interact', data)
+    return jsonify(reformat(result))
+
+
+def reformat(result):
     if result is not None:
         if 'command' in result:
             pass
         else:
             result['sparql2nl'] = sparql2nl(result['query'])
-            if result is not None and 'IO' in result and len(result['IO']['values']) == 0:
+            if 'IO' in result and len(result['IO']['values']) == 0:
                 result['IO']['surface'] = result['sparql2nl']
                 result['IO']['value'] = ['Correct?']
 
-    return jsonify(result)
-
+    return result
 
 @app.route('/correct')
 @login_required
