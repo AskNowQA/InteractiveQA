72af3305f2f4fff66a612b57af06a3c98814d725
hamidzafar <hamid.zafar.tud@gmail.com>
2019-01-30 18:29:43 +0100

    add hidden field to keep the task

--- UI/forms/submitQuestionForm.py
+++ UI/forms/submitQuestionForm.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, HiddenField
 from wtforms.validators import InputRequired
 
 
 class SubmitQuestionForm(FlaskForm):
     question = StringField(id='question', label='Question: ', validators=[InputRequired()])
+    task = HiddenField(id='task')
