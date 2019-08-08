3522fdd85da2d5de571d870ce60dd47449199d0f
hamidzafar <hamid.zafar.tud@gmail.com>
2019-08-08 12:15:16 +0200

    fix typo

--- common/component/linker/compositeLinker.py
+++ common/component/linker/compositeLinker.py
@@ -1,13 +1,13 @@
 class CompositeLinker:
     def __init__(self, entity_linker, relation_linker):
         self.entity_linker = entity_linker
-        self.relation_liner = relation_linker
+        self.relation_linker = relation_linker
 
     def link_entities(self, question, chunks=None):
         return self.entity_linker.link_entities(question, chunks)
 
     def link_relations(self, question, chunks=None):
-        return self.relation_liner.link_relations(question, chunks)
+        return self.relation_linker.link_relations(question, chunks)
 
     def link_entities_relations(self, question, chunks=None):
         return {"relations": self.link_relations(question, chunks),
