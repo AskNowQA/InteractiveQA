ad4f25347067fde4ea8aa81216094f6f22f9b9da
hamidzafar <hamid.zafar.tud@gmail.com>
2019-08-08 12:15:14 +0200

    add test+add connecting_relations param

--- common/component/linker/mdp.py
+++ common/component/linker/mdp.py
@@ -6,14 +6,16 @@ import os
 
 
 class MDP(Chunker):
-    def __init__(self, endpoint=config.config["MDP"]["endpoint"], cache_path="", use_cache=True):
+    def __init__(self, endpoint=config.config["MDP"]["endpoint"], cache_path="", use_cache=True,
+                 connecting_relations=False):
         self.endpoint = endpoint
+        self.connecting_relations = connecting_relations
         self.cache_path = cache_path
         self.use_cache = use_cache
         self.cache = {}
         if self.use_cache:
             Utils.makedirs(cache_path)
-            self.cache_path = os.path.join(cache_path, "mdp.cache")
+            self.cache_path = os.path.join(cache_path, "mdp-{}.cache".format(connecting_relations))
             self.__load_cache()
 
     def __load_cache(self):
@@ -33,7 +35,7 @@ class MDP(Chunker):
 
     def __hit_endpoint(self, question, chunks):
         id = question
-        input = {'question': question, 'k': 10}
+        input = {'question': question, 'k': 10, 'connecting_relations': self.connecting_relations}
 
         output = {'entities': [], 'relations': [], 'chunks': []}
         if id not in self.cache or not self.use_cache:
@@ -59,3 +61,9 @@ class MDP(Chunker):
     def link_entities_relations(self, question, chunks=None):
         return {"relations": self.link_relations(question, chunks),
                 "entities": self.link_entities(question, chunks)}
+
+
+if __name__ == '__main__':
+    print('MDP')
+    mdp = MDP('http://localhost:5006/link', '', False)
+    print(mdp.link_entities_relations('Who is the father of Barak Obama?'))
